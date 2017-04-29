3dcd232d3f4ebe1036fbc35fc5cb77bcfcad89ac
pebble8888 <pebble8888@gmail.com>
2017-04-29 11:52:52 +0900

    remove comment

--- cslib/PBLogger.cs
+++ cslib/PBLogger.cs
@@ -14,8 +14,6 @@ namespace cslib
         {
             StackTrace st = new StackTrace(1, true);
             string dateTime = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
-            //string fileName = Path.GetFileName(st.GetFrame(0).GetFileName());
-            //string lineNumber = st.GetFrame(0).GetFileLineNumber().ToString();
             var t = dateTime + " " + s;
             Debug.Print(t);
         }
